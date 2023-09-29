443c60ce458eaddaa416522c5a27ab42b521aa1f
piroping <pirogurama@gmail.com>
2023-09-30 08:23:02 +0900

    記録用紙更新

--- make_record_card.py
+++ make_record_card.py
@@ -22,9 +22,13 @@ class MakeRecord:
         font = ImageFont.truetype('C:/Windows/Fonts/BIZ-UDGothicR.ttc', 72)
         draw = ImageDraw.Draw(image)
         for i, (x, y) in zip(li, [(i, j) for i in range(2) for j in range(4)]):
-            draw.multiline_text((137 + x * 1309, 137 + y * 954), f'函館ラ・サール学園  速歩遠足\n{i[0]}年{i[1]}組{i[2]}番', fill=0, font=font)
+            draw.multiline_text((image.width // 2 * x + 180, image.height // 4 * y + 50), f'函館ラ・サール学園  速歩遠足\n{i[0]}年{i[1]}組{i[2]}番', fill=0, font=font)
             code = qrcode.make(f'{i[0]}{i[1]}{str(i[2]).zfill(2)}', error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 30) # type: ignore
-            image.paste(code, (137 + x * 1309 - 100, 137 + y * 954 + 150))
+            image.paste(code, (image.width // 2 * x - 100 + 180, image.height // 4 * y + 150 + 50))
+        
+        draw.line((image.width / 2, 0, image.width / 2, image.height), 0, width=8)
+        for i in range(1, 4):
+            draw.line((0, image.height / 4 * i, image.width, image.height / 4 * i), 0, width=8)
         
         image.save(f'records/{ind}.png')
 
